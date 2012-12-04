e96b3d43029d5a90d5ceced04ac331c4814a5fbd
Christopher Nelson <cnelson@jw-rbc.org>
2012-12-04 18:51:39 -0500

    Added coco builder for toolchain.

--- build_toolchain.py
+++ build_toolchain.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import sys
 
 from glob import glob
 
@@ -24,8 +25,11 @@ def install_pegtl():
    copy_tree(src_dir, install_dir)
    
 def install_coco():
+   print "Building coco"
    coco_dir = os.path.join(toolchain_dir, "src", "coco_cpp")
    install_dir = os.path.join(toolchain_dir, "bin")
+   if not os.path.exists(install_dir):
+      os.makedirs(install_dir)
    cur_dir = os.getcwd()
    os.chdir(coco_dir)
    exec_name = "coco" if not sys.platform.startswith("win") else "coco.exe"
@@ -33,9 +37,11 @@ def install_coco():
    if os.system("g++ *.cpp -o %s" % exec_name)!=0:
       sys.exit(1)
       
-   shutil.copyfile(os.path.join(coco_dir, exec_name), install_dir)
+   print "Installing coco"
+   shutil.copyfile(os.path.join(coco_dir, exec_name), os.path.join(install_dir, exec_name))
    for filename in glob(os.path.join(coco_dir, "*.frame")):
-      shutil.copyfile(filename, install_dir)   
+      dst_file = filename.replace(coco_dir, install_dir)
+      shutil.copyfile(filename, dst_file)   
    
 # Perform installation
 install_pegtl()
