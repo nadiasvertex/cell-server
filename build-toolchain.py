fd3e5a5fc6a9c256fd878abb5ef227316738c17a
Christopher Nelson <nadiasvertex@gmail.com>
2012-12-07 20:30:55 -0500

    Added ability to force-build a package.

--- build-toolchain.py
+++ build-toolchain.py
@@ -11,6 +11,9 @@ parser = argparse.ArgumentParser(description='Build the cell server toolchain')
 parser.add_argument('--list', dest='list_pkgs', action='store_true',
                    default=False,
                    help='List the available packages')
+parser.add_argument('--rebuild', dest='rebuild_pkgs', action='store',
+                   default="",
+                   help='Forces a rebuild of the specified packages.')
 parser.add_argument('--build', dest='build_pkgs', action='store',
                    default="",
                    help='Build all packages (the default), or specify a '\
@@ -31,6 +34,11 @@ if args.list_pkgs:
       print k.rjust(20), v[1].ljust(15),
       print ",".join(v[2]).rjust(40)
    sys.exit(0)
+   
+if args.rebuild_pkgs:
+   pkg_names = [x.strip() for x in args.rebuild_pkgs.split(",")]
+   for pkg_name in pkg_names:
+      mgr.set_status(pkg_name, "MISSING")
    
 if mgr.build(args.build_pkgs)==False:
    sys.exit(1)
