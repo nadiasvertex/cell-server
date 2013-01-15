50231269ccfbe1e0d45c670fb0201a279d5f7e8c
Christopher Nelson <nadiasvertex@gmail.com>
2013-01-15 17:47:29 -0500

    Added thread and GIL safety for Python interpreter calls.

--- src/lib/data_engine/cpp/py_engine.h
+++ src/lib/data_engine/cpp/py_engine.h
@@ -1,31 +1,69 @@
 #include <Python.h>
 #include <string>
 
-namespace cell {
-namespace engine {
+namespace cell
+{
+namespace engine
+{
 
 PyMODINIT_FUNC PyInit_native_data_engine(void);
 
 /** Provides a Python interpreter environment. */
 class py_engine
 {
+  static bool initialized;
+  PyThreadState *thread_state;
+
+  class py_guard
+  {
+     const py_engine& e;
+     PyGILState_STATE saved;
+     PyThreadState *old_thread_state;
+  public:
+     py_guard(const py_engine& _e):e(_e)
+     {
+        saved = PyGILState_Ensure();
+        old_thread_state = PyThreadState_Swap(e.thread_state);
+     }
+
+     ~py_guard()
+     {
+        PyThreadState_Swap(old_thread_state);
+        PyGILState_Release(saved);
+     }
+  };
+
+  friend class py_guard;
+
 public:
-   py_engine()
-   {
+  py_engine() {
+    if (!initialized) {
       PyImport_AppendInittab("native_data_engine",
                              &PyInit_native_data_engine);
       Py_Initialize();
-   }
+      initialized = true;
+    }
 
-   ~py_engine()
-   {
-      Py_Finalize();
-   }
+    thread_state = Py_NewInterpreter();
 
-   bool exec(const std::string& script)
-   {
-      return PyRun_SimpleString(script.c_str()) == 0;
-   }
+  }
+
+  ~py_engine() {
+     py_guard guard(*this);
+     Py_EndInterpreter(thread_state);
+
+  }
+
+  bool exec(const std::string& script) {
+    py_guard guard(*this);
+    return PyRun_SimpleString(script.c_str()) == 0;
+
+  }
+
+  bool test(const std::string& script) {
+     py_guard guard(*this);
+
+  }
 
 };
 
