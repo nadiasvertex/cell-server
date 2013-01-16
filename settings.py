e631add7a7076684e4bf2df1ea13485edb984113
Christopher Nelson <nadiasvertex@gmail.com>
2013-01-16 03:02:08 -0500

    Forgot settings files.

---
+++ settings.py
@@ -0,0 +1,23 @@
+clang={'CC': '/home/christopher/workspace/cell-server/toolchain/linux2/bin/cell-clang',
+ 'CFLAGS': '-I/home/christopher/workspace/cell-server/toolchain/linux2/include -target x86_64-cell-linux',
+ 'CPP': '/usr/bin/cpp',
+ 'CXX': '/home/christopher/workspace/cell-server/toolchain/linux2/bin/cell-clang++',
+ 'CXXCPP': '/usr/bin/cpp',
+ 'CXXFLAGS': '-I/home/christopher/workspace/cell-server/toolchain/linux2/include -target x86_64-cell-linux',
+ 'LD': 'ld',
+ 'LDFLAGS': '-L/home/christopher/workspace/cell-server/toolchain/linux2/lib -Wl,-rpath,/home/christopher/workspace/cell-server/toolchain/linux2/lib '}
+gcc={'CC': 'gcc',
+ 'CFLAGS': '-I/home/christopher/workspace/cell-server/toolchain/linux2/include',
+ 'CPP': '/usr/bin/cpp',
+ 'CXX': 'g++',
+ 'CXXCPP': '/usr/bin/cpp',
+ 'CXXFLAGS': '-I/home/christopher/workspace/cell-server/toolchain/linux2/include',
+ 'LD': 'ld',
+ 'LDFLAGS': '-L/home/christopher/workspace/cell-server/toolchain/linux2/lib -Wl,-rpath,/home/christopher/workspace/cell-server/toolchain/linux2/lib '}
+toolchain_bin_dirs=['/home/christopher/workspace/cell-server/toolchain/bin',
+ '/home/christopher/workspace/cell-server/toolchain/linux2/bin']
+toolchain_lib_dir='/home/christopher/workspace/cell-server/toolchain/linux2/lib'
+toolchain_include_dir='/home/christopher/workspace/cell-server/toolchain/linux2/include'
+toolchain_src_dir='/home/christopher/workspace/cell-server/toolchain/src'
+toolchain_dir='/home/christopher/workspace/cell-server/toolchain'
+toolchain_platform_dir='/home/christopher/workspace/cell-server/toolchain/linux2'
