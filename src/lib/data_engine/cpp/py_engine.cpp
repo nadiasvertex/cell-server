6493bc20bd2d40b334f34cff39fceb280bac4f7d
Christopher Nelson <nadiasvertex@gmail.com>
2013-01-19 06:59:59 -0500

    Added implementation for python binding.

---
+++ src/lib/data_engine/cpp/py_engine.cpp
@@ -0,0 +1,41 @@
+#include <data_engine/cpp/py_engine.h>
+
+namespace cell
+{
+namespace engine
+{
+
+  bool py_engine::initialized=false;
+
+  py_engine::py_engine() {
+    if (!initialized) {
+      PyImport_AppendInittab("native_data_engine",
+                             &PyInit_native_data_engine);
+      Py_Initialize();
+      initialized = true;
+    }
+    thread_state = Py_NewInterpreter();
+  }
+
+  py_engine::~py_engine() {
+     py_guard guard(*this);
+     Py_EndInterpreter(thread_state);
+
+  }
+
+  bool py_engine::exec(const std::string& script) {
+    py_guard guard(*this);
+    return PyRun_SimpleString(script.c_str()) == 0;
+  }
+
+  bool py_engine::test(const std::string& script) {
+     py_guard guard(*this);
+     auto result = PyRun_String(script.c_str(),
+				Py_eval_input,  
+				PyEval_GetGlobals(),
+				PyEval_GetLocals());
+  }
+
+
+} // end namespace engine
+} // end namespace cell
--- src/lib/data_engine/cpp/py_engine.h
+++ src/lib/data_engine/cpp/py_engine.h
@@ -17,54 +17,27 @@ class py_engine
   class py_guard
   {
      const py_engine& e;
-     PyGILState_STATE saved;
      PyThreadState *old_thread_state;
   public:
      py_guard(const py_engine& _e):e(_e)
      {
-        saved = PyGILState_Ensure();
         old_thread_state = PyThreadState_Swap(e.thread_state);
      }
 
      ~py_guard()
      {
         PyThreadState_Swap(old_thread_state);
-        PyGILState_Release(saved);
      }
   };
 
   friend class py_guard;
 
 public:
-  py_engine() {
-    if (!initialized) {
-      PyImport_AppendInittab("native_data_engine",
-                             &PyInit_native_data_engine);
-      Py_Initialize();
-      initialized = true;
-    }
+  py_engine();
+  ~py_engine();
 
-    thread_state = Py_NewInterpreter();
-
-  }
-
-  ~py_engine() {
-     py_guard guard(*this);
-     Py_EndInterpreter(thread_state);
-
-  }
-
-  bool exec(const std::string& script) {
-    py_guard guard(*this);
-    return PyRun_SimpleString(script.c_str()) == 0;
-
-  }
-
-  bool test(const std::string& script) {
-     py_guard guard(*this);
-
-  }
-
+  bool exec(const std::string& script);
+  bool test(const std::string& script);
 };
 
 } // end namespace engine
